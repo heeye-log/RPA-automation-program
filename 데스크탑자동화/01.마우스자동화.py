353154b801e7dbcff337b285133ab447a98e386e
HYEYEON KIM <121289071+heeye-log@users.noreply.github.com>
2023-01-26 23:08:41 +0900

    Create 01.마우스자동화.py

---
+++ 데스크탑자동화/01.마우스자동화.py
@@ -0,0 +1,20 @@
+import pyautogui
+import time
+
+# 화면 크기 출력
+# print(pyautogui.size())
+
+# 마우스 위치 출력
+# time.sleep(2)
+# print(pyautogui.position())
+
+# 마우스 이동
+# mac = 손쉬운 사용 vscode 권한 설정
+# pyautogui.moveTo(164,362)
+pyautogui.moveTo(164,362,2)
+
+# 마우스 클릭
+pyautogui.click()
+pyautogui.click(button='right')
+pyautogui.doubleClick()
+pyautogui.click(clicks=3,interval=1) #3번 클릭하는데 1초마다 하기
