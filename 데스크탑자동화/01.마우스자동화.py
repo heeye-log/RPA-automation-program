7bea8ac1efff6afa446046b0681cbcc7ff59d906
HYEYEON KIM <121289071+heeye-log@users.noreply.github.com>
2023-01-28 16:02:17 +0900

    Update 01.마우스자동화.py

--- 데스크탑자동화/01.마우스자동화.py
+++ 데스크탑자동화/01.마우스자동화.py
@@ -2,15 +2,15 @@ import pyautogui
 import time
 
 # 화면 크기 출력
-# print(pyautogui.size())
+print(pyautogui.size())
 
 # 마우스 위치 출력
-# time.sleep(2)
-# print(pyautogui.position())
+time.sleep(2)
+print(pyautogui.position())
 
 # 마우스 이동
-# mac = 손쉬운 사용 vscode 권한 설정
-# pyautogui.moveTo(164,362)
+mac = 손쉬운 사용 vscode 권한 설정
+pyautogui.moveTo(164,362)
 pyautogui.moveTo(164,362,2)
 
 # 마우스 클릭
