886d8bf01316e914f1e56b5ad432beafdbe6f092
HYEYEON KIM <121289071+heeye-log@users.noreply.github.com>
2023-01-28 16:02:40 +0900

    Update 02.키보드자동화.py

--- 데스크탑자동화/02.키보드자동화.py
+++ 데스크탑자동화/02.키보드자동화.py
@@ -2,14 +2,14 @@ import pyautogui
 import pyperclip
 
 # 키보드 입력(문자) / 한글 미지원
-# pyautogui.write('startcoding',interval=0.25)
+pyautogui.write('startcoding',interval=0.25)
 
 # 키보드 입력(키)
-# pyautogui.press('enter')
-# pyautogui.press('up')
+pyautogui.press('enter')
+pyautogui.press('up')
 
 # 키보드 입력(여러개 동시에)
-# pyautogui.hotkey('command','c')
+pyautogui.hotkey('command','c')
 
 # 한글 입력 방법
 pyperclip.copy('한글입력')
